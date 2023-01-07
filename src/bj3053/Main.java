72aced27c7a1e2cacf20ab5c4df396e824d6cf44
TaeYeongKwak <abgudv6403@naver.com>
2023-01-07 10:36:32 +0900

    20230107

---
+++ src/bj3053/Main.java
@@ -0,0 +1,13 @@
+package bj3053;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+
+        System.out.println(r * r * Math.PI);
+        System.out.println(r * r * 2);
+    }
+}
